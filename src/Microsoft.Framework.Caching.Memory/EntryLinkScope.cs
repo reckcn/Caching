53e9ebd927dbf31a438fbd76eeae4736b0627898
Kiran Challa <kichalla@microsoft.com>
2015-05-13 17:37:00 -0700

    added missing file

---
+++ src/Microsoft.Framework.Caching.Memory/EntryLinkScope.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.Framework.Caching.Memory
+{
+    public class EntryLinkScope : IDisposable
+    {
+        public EntryLinkScope()
+        {
+            Parent = EntryLinkHelpers.CurrentScope;
+            EntryLink = new EntryLink();
+        }
+
+        public EntryLinkScope Parent { get; }
+
+        public IEntryLink EntryLink { get; }
+
+        private bool _disposedValue = false; // To detect redundant calls
+        public void Dispose()
+        {
+            if (!_disposedValue)
+            {
+                RemoveScope();
+            }
+            _disposedValue = true;
+        }
+
+        private void RemoveScope()
+        {
+            for (var scope = EntryLinkHelpers.CurrentScope; scope != null; scope = scope.Parent)
+            {
+                if (ReferenceEquals(scope, this))
+                {
+                    EntryLinkHelpers.CurrentScope = Parent;
+                }
+            }
+        }
+    }
+}
